1084b4d5e76759225c95613b67806683681b3aa9
7634077 <95167233+7634077@users.noreply.github.com>
2022-08-03 01:07:12 +0300

    tuple

--- Tuple.cs
+++ Tuple.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace C_SHARP
 {
-    class Tuple
+    //אוסף של משתנים שאפשר להעביר אותם בתור פרמטר אחד
+    internal class Tuple
     {
+        public void run()
+        {
+            int id;
+            string name;
+            (int id,string fname,string lname ) var1;
+            var1 = (123,"Moshe","Cohen");
+        }
     }
 }
